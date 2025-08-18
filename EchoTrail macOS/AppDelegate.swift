7750b5c77dd60a87ef9ba28222b9ac23e4126123
Tianyue Qi <taylor_chyi@icloud.com>
2025-08-18 23:53:47 +0800

    Initial Commit

---
+++ EchoTrail macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  EchoTrail macOS
+//
+//  Created by 齐天乐 on 2025/8/18.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
